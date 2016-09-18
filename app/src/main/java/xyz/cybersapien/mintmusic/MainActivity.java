17c6c3be424d5ef892371b838dfb7f0f87139331
ogcybersapien <aditya@cybersapien.xyz>
2016-09-18 21:53:53 +0530

    Added additional activities to the app. Need to add details about the my account and playlist detail activities and add more refinement. Then decide if we need any additional activities before finalizing.

--- app/src/main/java/xyz/cybersapien/mintmusic/MainActivity.java
+++ app/src/main/java/xyz/cybersapien/mintmusic/MainActivity.java
@@ -1,6 +1,7 @@
 package xyz.cybersapien.mintmusic;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +15,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToNowPlaying(View v){
-        Intent intent = new Intent(this,NowPlaying.class);
-        //We'll need to add some data to the intent as and when required use .setData() Method.
-        startActivity(intent);
+        Intent nowPlayingIntent = new Intent(this,NowPlaying.class);
+        //We'll need to add some data to the nowPlayingIntent as and when required use .setData() Method.
+        startActivity(nowPlayingIntent);
+    }
+
+    public void goToPlayListDetails(View view){
+        Intent playlistIntent = new Intent(this, playlistDetail.class);
+        //Pass the name of the playlist using .setData() method
+        startActivity(playlistIntent);
+    }
+
+    public void goToMyAccount(View v){
+        Intent myAccountIntent = new Intent(this, myaccount.class);
+        startActivity(myAccountIntent);
     }
 }
